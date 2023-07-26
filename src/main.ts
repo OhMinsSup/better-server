508ed35ae08fd0c40de4595409ef5697fcdf4980
OhMinSeop <msoh@2online.io>
2023-07-26 18:29:09 +0900

    add: init 프로젝트

---
+++ src/main.ts
@@ -0,0 +1,58 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import helmet from '@fastify/helmet';
+import fastifyCsrf from '@fastify/csrf-protection';
+import compression from '@fastify/compress';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  );
+
+  // Apply validation pipes globally
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = app.get(ConfigService);
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      const allowedHosts = [/^https:\/\/domain.io$/];
+      if (config.get('NODE_ENV') === 'development') {
+        allowedHosts.push(/^http:\/\/localhost/);
+      }
+
+      let corsOptions: any;
+      const valid = allowedHosts.some((regex) => regex.test(origin));
+      if (valid) {
+        corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+      } else {
+        corsOptions = { origin: false }; // disable CORS for this request
+      }
+      callback(null, corsOptions);
+    },
+    credentials: true,
+  });
+
+  await app.register(helmet);
+  await app.register(fastifyCsrf);
+  await app.register(compression);
+
+  await app.listen(8080, '0.0.0.0');
+}
+
+bootstrap();
